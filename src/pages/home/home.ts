dcd1edf7093b98381e3ab251a23859f2b7f2b22c
Leandro M Macedo <lmacedo@scopus.com.br>
2017-10-20 17:37:02 -0200

    New mapping on foods + New mapping on imperial cooking + Food's names changed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -36,9 +36,14 @@ export class HomePage {
       {"id":"16","name":"Molho Branco","icon":"assets/img/foodAndIgredients/whiteSauce.png","hasIngredientPage":false},       
       {"id":"17","name":"Oleo de Oliva","icon":"assets/img/foodAndIgredients/oliveOil.png","hasIngredientPage":false},       
       {"id":"18","name":"Massa","icon":"assets/img/foodAndIgredients/rough.png","hasIngredientPage":false},
+      {"id":"19","name":"Plantas","icon":"assets/img/foodAndIgredients/rose.png","hasIngredientPage":false},
+      {"id":"20","name":"Frutas","icon":"assets/img/foodAndIgredients/apple.png","hasIngredientPage":false},
+      {"id":"21","name":"Alho","icon":"assets/img/foodAndIgredients/garlic.png","hasIngredientPage":false},
+      {"id":"22","name":"Molho vermelho","icon":"assets/img/foodAndIgredients/redSauce.png","hasIngredientPage":false},
+      {"id":"23","name":"Creme","icon":"assets/img/foodAndIgredients/cream.png","hasIngredientPage":false},
   	];
     sessionStorage.setItem('ingredients', JSON.stringify(ingredients));
-    
+
     let foodList = [
       {"id":"0", "name":"Cerveja", "level":"Iniciante 1", "ingredients":[{"id":"0", "qty":5},{"id":"1", "qty":6},{"id":"2", "qty": 1},{"id":"6", "qty": 2}], "icon":"assets/img/foodAndIgredients/beer.png","plusCrafted":[{"name":"Cerveja Gelada"}]},
       {"id":"1", "name":"Ração Orgânica", "level":"Aprendiz 6", "ingredients":[{"id":"3", "qty":2},{"id":"4", "qty":5},{"id":"7", "qty": 4},{"id":"5", "qty": 4}], "icon":"assets/img/foodAndIgredients/organicFeedA.png"},
@@ -46,9 +51,12 @@ export class HomePage {
       {"id":"3", "name":"Aveia", "level":"Aprendiz 1","ingredients":[{"id":"8","qty":9},{"id":"9","qty":3},{"id":"10","qty":3},{"id":"11","qty":2}],"icon":"assets/img/foodAndIgredients/oatmealA.png", "plusCrafted":[{"name":"Aveia Refinada"}]},
       {"id":"4", "name":"Peixe Frito", "level":"Aprendiz 1","ingredients":[{"id":"8","qty":3},{"id":"5","qty":2},{"id":"15","qty":2}],"icon":"assets/img/foodAndIgredients/friedFish.png", "plusCrafted":[{"name":"Peixe Frito Crocante"}]},
       {"id":"5", "name":"Salgado de File de Peixe", "level":"Aprendiz 1","ingredients":[{"id":"5","qty":2},{"id":"16","qty":3},{"id":"8","qty":7},{"id":"13","qty":2}],"icon":"assets/img/foodAndIgredients/fishFilletChips.png", "plusCrafted":[{"name":"Salgado de File de Peixe Delicioso"}]},
-      {"id":"6", "name":"Ensopado de Carne", "level":"Iniciante 1","ingredients":[{"id":"4","qty":5},{"id":"8","qty":2},{"id":"12","qty":2},{"id":"1","qty":3}],"icon":"assets/img/foodAndIgredients/meatStew.png", "plusCrafted":[{"name":"Ensopado de Carne Forte"}]},
+      {"id":"6", "name":"Sopa de Carne", "level":"Iniciante 1","ingredients":[{"id":"4","qty":5},{"id":"8","qty":2},{"id":"12","qty":2},{"id":"1","qty":3}],"icon":"assets/img/foodAndIgredients/meatStew.png", "plusCrafted":[{"name":"Ensopado de Carne Forte"}]},
       {"id":"7", "name":"Omelete", "level":"Aprendiz 1","ingredients":[{"id":"0","qty":5},{"id":"17","qty":2},{"id":"14","qty":5},{"id":"13","qty":2}],"icon":"assets/img/foodAndIgredients/omelete.png", "plusCrafted":[{"name":"Omelete Macio"}]},
       {"id":"8", "name":"Biscoito de Mel", "level":"Aprendiz 6","ingredients":[{"id":"11","qty":6},{"id":"14","qty":2},{"id":"9","qty":4},{"id":"18","qty":2}],"icon":"assets/img/foodAndIgredients/honeycombCookie.png", "plusCrafted":[{"name":"Biscoito de Mel Crocante"}]},
+      {"id":"9", "name":"Chá com Aroma Fino", "level":"Aprendiz 1", "ingredients":[{"id":"19","qty":4},{"id":"20","qty":4},{"id":"1","qty":7},{"id":"11","qty":3}],"icon":"assets/img/foodAndIgredients/teaWithFineScent.png", "plusCrafted":[{"name":"Chá com Aroma Fino Forte"}]},
+      {"id":"10", "name":"Bife", "level":"Aprendiz 1", "ingredients":[{"id":"4","qty":8},{"id":"13","qty":2},{"id":"21","qty":2},{"id":"22","qty":2}],"icon":"assets/img/foodAndIgredients/steak.png", "plusCrafted":[{"name":"Bife Suculento"}]},
+      {"id":"11", "name":"Pudim de Frutas", "level":"Iniciante 6", "ingredients":[{"id":"20","qty":5},{"id":"23","qty":1},{"id":"9","qty":3},{"id":"2","qty":2}],"icon":"assets/img/foodAndIgredients/fruitPudding.png", "plusCrafted":[{"name":"Pudim de Frutas Especial"}]},
     ];
     sessionStorage.setItem('foodList', JSON.stringify(foodList));
 
@@ -59,18 +67,28 @@ export class HomePage {
       {"id":"3", "name":"Aveia", "icon":"assets/img/foodAndIgredients/oatmealA.png", "hasFoodPage":true},
       {"id":"4", "name":"Peixe Frito", "icon":"assets/img/foodAndIgredients/friedFish.png", "hasFoodPage":true},
       {"id":"5", "name":"Salgado de File de Peixe", "icon":"assets/img/foodAndIgredients/fishFilletChips.png", "hasFoodPage":true},
-      {"id":"6", "name":"Ensopado de Carne", "icon":"assets/img/foodAndIgredients/meatStew.png", "hasFoodPage":true},
+      {"id":"6", "name":"Sopa de Carne", "icon":"assets/img/foodAndIgredients/meatStew.png", "hasFoodPage":true},
       {"id":"7", "name":"Omelete", "icon":"assets/img/foodAndIgredients/omelete.png", "hasFoodPage":true},
       {"id":"8", "name":"Biscoito de Mel","icon":"assets/img/foodAndIgredients/honeycombCookie.png", "hasFoodPage":true},
+      {"id":"9", "name":"Chá com Aroma Fino","icon":"assets/img/foodAndIgredients/teaWithFineScent.png", "hasFoodPage":true},
+      {"id":"10", "name":"Bife","icon":"assets/img/foodAndIgredients/steak.png", "hasFoodPage":true},
+      {"id":"11", "name":"Pudim de Frutas","icon":"assets/img/foodAndIgredients/fruitPudding.png", "hasFoodPage":true},
     ];
     sessionStorage.setItem('foods', JSON.stringify(foods));  
 
     let imperialCookingList = [
-  		{"id":"0","name":"Caixa de Peixe Frito", "icon":"assets/img/imperialItems/friedFishCookingBox.png","level":"Aprendiz 1","local":"Heidel, Altinova e Calpheon","cratfItemId":"4","qtyToCraft":20, "hasFoodPage":true, "price":150000},
-      {"id":"1","name":"Caixa Ovo Cozido", "icon":"assets/img/imperialItems/boiledEggCookingBox.png","level":"Aprendiz 1","local":"Heidel, Altinova e Calpheon","cratfItemId":"2","qtyToCraft":20, "hasFoodPage":true, "price":138750},
-      {"id":"2","name":"Caixa de Salgado de File de Peixe", "icon":"assets/img/imperialItems/fishFilletCookingBox.png","level":"Proficiente 1","local":"Heidel, Altinova e Calpheon","cratfItemId":"5","qtyToCraft":20, "hasFoodPage":true, "price":160000},
-      {"id":"3","name":"Caixa de Ensopado de Carne", "icon":"assets/img/imperialItems/meatStewCookingBox.png","level":"Aprendiz 1","local":"Heidel, Altinova e Calpheon","cratfItemId":"6","qtyToCraft":20, "hasFoodPage":true, "price":195000},
-      {"id":"4","name":"Caixa de Omelete", "icon":"assets/img/imperialItems/omeleteCookingBox.png","level":"Proficiente 1","local":"Heidel, Altinova e Calpheon","cratfItemId":"7","qtyToCraft":40, "hasFoodPage":true, "price":205000},
+  		{"id":"0","name":"Baú de Peixe Frito", "icon":"assets/img/imperialItems/friedFishCookingBox.png","level":"Aprendiz","local":"Heidel, Altinova e Calpheon","cratfItemId":"4","qtyToCraft":20, "hasFoodPage":true, "price":150000},
+      {"id":"1","name":"Baú de Ovo Cozido de Pássaro", "icon":"assets/img/imperialItems/boiledEggCookingBox.png","level":"Aprendiz","local":"Heidel, Altinova e Calpheon","cratfItemId":"2","qtyToCraft":20, "hasFoodPage":true, "price":138750},      
+      {"id":"2","name":"Baú de Sopa de Carne", "icon":"assets/img/imperialItems/meatStewCookingBox.png","level":"Aprendiz","local":"Heidel, Altinova e Calpheon","cratfItemId":"6","qtyToCraft":20, "hasFoodPage":true, "price":195000},
+      {"id":"6","name":"Baú de File de Peixe", "icon":"assets/img/imperialItems/fishFilletCookingBox.png","level":"Proficiente","local":"Heidel, Altinova e Calpheon","cratfItemId":"5","qtyToCraft":20, "hasFoodPage":true, "price":160000},
+      {"id":"6","name":"Baú de Pudim de Fruta", "icon":"assets/img/imperialItems/fruitPuddingCookingBox.png","level":"Proficiente","local":"Heidel, Altinova e Calpheon","cratfItemId":"11","qtyToCraft":20, "hasFoodPage":true, "price":212500},
+      {"id":"3","name":"Baú de Chá com Aroma Fino", "icon":"assets/img/imperialItems/goodSmellingCookingBox.png","level":"Profissional","local":"Heidel, Altinova e Calpheon","cratfItemId":"9","qtyToCraft":30, "hasFoodPage":true, "price":217500},
+      {"id":"4","name":"Baú de Omelete", "icon":"assets/img/imperialItems/omeleteCookingBox.png","level":"Profissional","local":"Heidel, Altinova e Calpheon","cratfItemId":"7","qtyToCraft":40, "hasFoodPage":true, "price":205000},
+      {"id":"5","name":"Baú de Carne", "icon":"assets/img/imperialItems/steakCookingBox.png","level":"Profissional","local":"Heidel, Altinova e Calpheon","cratfItemId":"10","qtyToCraft":20, "hasFoodPage":true, "price":242500},      
+     
+      
+      
+
   	];
 	sessionStorage.setItem('imperialCookingList', JSON.stringify(imperialCookingList));    	
   }
